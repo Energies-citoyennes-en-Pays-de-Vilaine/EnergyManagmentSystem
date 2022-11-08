daad13992aaf52ed9531a15bfff35cbf2dfb6178
laennocode <laennocode@gmail.com>
2022-11-08 09:53:34 +0100

    removed unused files

--- learning/teest.py
+++
@@ -1,9 +0,0 @@
-from file_reader import FileReader
-from datetime import datetime
-import matplotlib.pyplot as plt
-fr = FileReader("../testData/courbe_SL.csv")
-curve = fr.readData(0)
-
-print(curve[:,0])
-plt.plot(curve[:,0],curve[:,1])
-plt.show()
