c4e7f82107d12c6274f5ebf9c367fde9fa513539
laennocode <laennocode@gmail.com>
2022-11-08 10:07:26 +0100

    add of zabbix progress to repo with credentials cleanup done

---
+++ learning/zabbix_example.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from learning.zabbix_reader import ZabbixReader
+import numpy as np
+import matplotlib.pyplot as plt
+
+from learning.zabbix_credentials import credentials
+zr = ZabbixReader("http://mqtt.projet-elfe.fr/api_jsonrpc.php", credentials["username"], credentials["password"])
+zr.get_token()
+items = zr.get_items()
+i = 0
+for item in items:
+	print(i, item)
+	i = i +1
+#key = 47#64#15
+keyName = "A004 puissance"
+key=list(items.keys()).index(keyName)
+print(list(items.keys())[key])
+def average(data, period) :
+	current_date = int(data["timestamps"][-1])
+	current_count = 0
+	current_sum = 0
+	toReturn = {"timestamps" : [], "values" : []}
+	for i in reversed(range(len(data["timestamps"]))):
+		if (int(data["timestamps"][i]) > current_date + period):
+			toReturn["timestamps"].append(int(current_date))
+			toReturn["values"].append(current_sum / current_count)
+			current_count = 0
+			current_sum = 0
+			current_date = int(data["timestamps"][i])
+		current_sum += data["values"][i]
+		current_count += 1
+	if (current_count != 0):
+		toReturn["timestamps"].append(current_date)
+		toReturn["values"].append(current_sum / current_count)
+	return toReturn
+
+current_timestamp = int(datetime.now().timestamp())
+data = zr.readData(items[list(items.keys())[key]], current_timestamp - 5*24*60*60, current_timestamp)
+data2 = average(data, 15*60)
+plt.plot([int(d) for d in data["timestamps"]], data["values"])
+plt.plot(data2["timestamps"], np.array(data2["values"]))
+plt.show()
+
+"""for key in list(items.keys()):
+	try:
+		data = zr.readAllData(items[key])
+		with open("data/" + key, "w") as outp:
+			for i in reversed(range(len(data["timestamps"]))):
+				print(data["timestamps"][i], data["values"][i], sep=" ", file=outp)
+	except:
+		continue"""
---
+++ learning/zabbix_reader.py
@@ -0,0 +1,86 @@
+import requests
+from learning.data_reader_interface import DataReaderInterface
+from datetime import datetime
+import json
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+class ZabbixReader(DataReaderInterface):
+	url : str
+	username : str
+	password : str
+	token : str
+	last_refresh : datetime
+
+	def __init__(self, url, username, password) -> None:
+		self.url = url
+		self.username = username
+		self.password = password
+		self.last_refresh = None
+	def send_request(self, method, params, has_token : bool = False, log : bool = False):
+		toSendObject = {
+			"jsonrpc": "2.0",
+			"method": method,
+			"params": params,
+			"id": 42,
+		}
+		if (has_token):
+			toSendObject["auth"] = self.get_token()
+		data = requests.post(self.url, json=toSendObject, headers={'Content-type': 'application/json'})
+		if (log):
+			print(params, data)
+		#TODO add error code management
+		data = json.loads(data.content)
+		return data
+	def get_token(self):
+		if (self.last_refresh == None or (datetime.now() - self.last_refresh).total_seconds() > 100):
+			print("refreshing token")
+			data = self.send_request("user.login", {
+				"user": self.username,
+				"password": self.password
+			},
+			False)
+			token = data["result"]
+			self.token = token
+			self.last_refresh = datetime.now()
+		return self.token
+	def get_items(self):
+		data = self.send_request("item.get", {}, True)["result"]
+		items = {}
+		for d in data:
+			if (re.search("[ADF]{1}\\d{1,3}", d["name"][:4]) != None):
+				items[d["name"]] = d["itemid"]
+		return items
+	def readData(self, clientID, time_from, time_till) -> np.ndarray:
+		data = self.send_request("history.get", {
+			"itemids" : clientID,
+			"history": 0,
+			"time_from" : time_from,
+			"time_till" : time_till,
+			"sortfield" : "clock",
+			"sortorder" : "DESC"
+		}, True)
+		toReturn = { 
+			"timestamps"  : [],
+			"values"      : []
+			}
+		for d in data["result"]:
+			toReturn["timestamps"].append(d["clock"])
+			toReturn["values"].append(float(d["value"]))
+		return toReturn
+	def readAllData(self, clientID) -> np.ndarray:
+		data = self.send_request("history.get", {
+			"itemids" : clientID,
+			"history": 0,
+			"sortfield" : "clock",
+			"sortorder" : "DESC",
+			"limit" : 100_000,
+		}, True, True)
+		toReturn = { 
+			"timestamps"  : [],
+			"values"      : []
+			}
+		for d in data["result"]:
+			toReturn["timestamps"].append(d["clock"])
+			toReturn["values"].append(float(d["value"]))
+		return toReturn
