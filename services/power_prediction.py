0307f46b17883917751245e82ba495ddbfa2cb2d
laennocode <laennocode@gmail.com>
2022-12-12 13:31:34 +0100

    [feature] add history-based prediction

---
+++ services/power_prediction.py
@@ -0,0 +1,32 @@
+from learning.database.EMS_db_types import EMSPowerCurveData
+from learning.database.EMS_db_creator import execute_queries
+from learning.zabbix_reader import ZabbixReader
+from credentials.db_credentials import db_credentials
+from credentials.zabbix_credentials import zabbix_credentials
+from datetime import datetime
+from learning.curve import get_full_curve
+import matplotlib.pyplot as plt
+
+CURVE_KEY        = "Equilibre General P=C bis"
+CURVE_PERIOD     = 15 * 60
+DAY_DURATION     = 24 * 60 * 60
+CURVE_DATA_TABLE = "prediction"
+zr = ZabbixReader("http://mqtt.projet-elfe.fr/api_jsonrpc.php", zabbix_credentials["username"], zabbix_credentials["password"])
+zr.get_token()
+
+items = zr.get_items()
+item_id = items[CURVE_KEY]
+current_timestamp = int(datetime.now().timestamp())
+data = zr.readData(item_id, current_timestamp - DAY_DURATION, current_timestamp)
+base_timestamp = data["timestamps"][0] - (data["timestamps"][0] % CURVE_PERIOD)
+curve = get_full_curve(data["timestamps"], data["values"], CURVE_PERIOD, 0)
+curve_base_timstamps = [base_timestamp + i * CURVE_PERIOD for i in range(len(curve))]
+
+data_to_post_to_database = []
+for i in range(len(curve)):
+	curve_data = EMSPowerCurveData(curve_base_timstamps[i] + DAY_DURATION, curve[i])
+	data_to_post_to_database.append(curve_data.get_create_or_update_in_table_str(CURVE_DATA_TABLE))
+for i in range(len(curve)):
+	curve_data = EMSPowerCurveData(curve_base_timstamps[i] + 2 * DAY_DURATION, curve[i])
+	data_to_post_to_database.append(curve_data.get_create_or_update_in_table_str(CURVE_DATA_TABLE))
+execute_queries(db_credentials["EMS"], data_to_post_to_database)
