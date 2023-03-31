8635d3405c4d1e5cee90f4613f561e421a156331
laennocode <laennocode@gmail.com>
2023-03-31 08:35:30 +0200

    update of VehicleConsumer

--- solution/ConsumerTypes/VehicleConsumer.py
+++ solution/ConsumerTypes/VehicleConsumer.py
@@ -4,6 +4,7 @@ from typing import *
 from solution.Consumer_interface import Consumer_interface
 from solution.Calculation_Params import CalculationParams
 from solution.Utils.utils import maxi, mini
+from math import ceil
 from typing import TypedDict
 class _CalculatedTimeParameters(TypedDict):
 	start_time          : int
@@ -12,6 +13,7 @@ class _CalculatedTimeParameters(TypedDict):
 	max_charge_time     : int
 	end_time_min_charge : int
 	end_time_max_charge : int
+	steps_count         : int
 class VehicleConsumer(Consumer_interface):
 	id : int # Elfe_Equipement_pilote_id
 	power_watt : int
@@ -47,59 +49,84 @@ class VehicleConsumer(Consumer_interface):
 		return [[1], [1]]
 	
 	def _get_calculated_time_parameters(self, calculationParams: CalculationParams) -> _CalculatedTimeParameters:
+		step_size         = calculationParams.step_size
 		start_time        = maxi(self.start_time, calculationParams.begin)
 		end_time          = mini(self.end_time, calculationParams.end)
-		min_charge_time   = 3600.0 * self.capacity_watt_hour * (self.end_charge_pourc - self.initial_charge_pourc) / (100.0 * self.power_watt)
-		max_charge_time   = 3600.0 * self.capacity_watt_hour * (self.end_charge_pourc - self.initial_charge_pourc) / (100.0 * self.power_watt)
+		min_charge_time   = step_size * ceil(3600.0 * self.capacity_watt_hour * (self.end_charge_pourc - self.initial_charge_pourc) / (100.0 * self.power_watt) / step_size)
+		max_charge_time   = step_size * ceil(3600.0 * self.capacity_watt_hour * (100 - self.initial_charge_pourc) / (100.0 * self.power_watt) / step_size)
 		end_time_min_charge = start_time + min_charge_time
 		end_time_max_charge = start_time + max_charge_time
+		steps_count         =  ceil((end_time - start_time - min_charge_time) / calculationParams.step_size)
 		return {
 			"start_time" : start_time,
 			"end_time": end_time,
 	  		"end_time_min_charge" : end_time_min_charge,
 			"end_time_max_charge" : end_time_max_charge,
 			"max_charge_time" : max_charge_time,
-			"min_charge_time" : min_charge_time
+			"min_charge_time" : min_charge_time,
+			"steps_count"     : steps_count
 			}
 	def _make_machine_possible(self, calculationParams: CalculationParams):
 		tp = self._get_calculated_time_parameters(calculationParams)
-		start_time = maxi(self.start_time, calculationParams.begin)
-		end_time = mini(self.end_time, calculationParams.end + calculationParams.step_size)
-		if (start_time + len(self.profile) * calculationParams.time_delta  > end_time ):
-			print(f"warning, machine {self.id} impossible because user constraints doesn't allow it to fit, rescheduling end constraint")
-			self.end_time = self.start_time + len(self.profile) * calculationParams.time_delta
-		if (start_time + len(self.profile) * calculationParams.time_delta > calculationParams.end):
-			print(f"warning, machine {self.id} impossible because it doesn't fit before the end of simulation. making it earlier so it fits, will be rescheduled later")
-			self.start_time = calculationParams.end - len(self.profile) * calculationParams.time_delta
-		if (self.end_time <= calculationParams.begin):
-			print(f"warning, machine {self.id} impossible because it's supposed to end before the start of simulation. scheduling it for next step")
+		start_time = tp["start_time"]
+		end_time = tp["end_time"]
+		if (start_time + tp["min_charge_time"]  > end_time ):
+			print(f"warning, electric vehicle {self.id} is impossible because user constraints doesn't allow it to fit, rescheduling end constraint")
+			self.start_time = tp["start_time"]
+			self.end_time   = self.start_time + tp["min_charge_time"]
+			tp              = self._get_calculated_time_parameters(calculationParams)
+		if (start_time + tp["min_charge_time"] > calculationParams.end):
+			print(f"warning, electric vehicle {self.id} is impossible because it doesn't fit before the end of simulation. making it earlier so it fits, will be rescheduled later")
+			self.end_time   = tp["end_time"]
+			self.start_time = self.end_time - tp["min_charge_time"]
+			tp              = self._get_calculated_time_parameters(calculationParams)
+		if (end_time <= calculationParams.begin):
+			print(f"warning, electric vehicle {self.id} is impossible because it's supposed to end before the start of simulation. scheduling it for next step")
 			self.start_time = calculationParams.begin
-			self.end_time = self.start_time + len(self.profile) * calculationParams.time_delta
+			self.end_time   = self.start_time + tp["end_time_min_charge"]
 
 	def _get_minimizing_variables_count(self, calculationParams : CalculationParams) -> int:
 		self._make_machine_possible(calculationParams)
-		start_time = maxi(self.start_time, calculationParams.begin)
-		end_time = mini(self.end_time, calculationParams.end + calculationParams.step_size)
-		steps_count = (end_time - start_time) / calculationParams.step_size
-		steps_count -= len(self.profile)
-		steps_count = round(steps_count)
+		tp = self._get_calculated_time_parameters(calculationParams)
+		steps_count = tp["steps_count"]
 		if steps_count < 0:
-			print(steps_count, start_time, end_time, calculationParams.step_size)
+			print(steps_count, tp, calculationParams.step_size)
 		return steps_count + 1
 
 	def _get_constraints_size(self, calculationParams : CalculationParams) -> int:
 		return 1 # only a sum constraint
 
+	def _calcul_liste_puissance_charge(self, calculationParams : CalculationParams):#intergration de courbe possible
+		#Code fournit par Thomas BREMBILLA, Clement EGO et Mathis AUBERT dans le cadre des mini-projets de 1ère année à l'ENS
+		pourcent_init = self.initial_charge_pourc
+		nb_pourcent = 100 - pourcent_init
+		valeur_pas = self.power_watt * calculationParams.step_size / 3600 #3600 secondes dans une heure
+		pourcent_par_pas = 100 * valeur_pas / self.capacity_watt_hour
+		nb_pas = int(nb_pourcent//pourcent_par_pas)
+		P = [-self.power_watt for k in range(nb_pas)]
+		residue = self.power_watt * (nb_pourcent - pourcent_par_pas * nb_pas)/pourcent_par_pas
+		if (residue >= 0.1):
+			P += [ - self.power_watt * (nb_pourcent - pourcent_par_pas * nb_pas)/pourcent_par_pas]
+		print(P)
+		return(P)
+
 	def _fill_minimizing_constraints(self, calculationParams: CalculationParams, tofill: np.ndarray, xpars: List[int], ypars: List[int]):
-		self._make_machine_possible(calculationParams) 
+		self._make_machine_possible(calculationParams)
+		tp = self._get_calculated_time_parameters(calculationParams)
 		sim_size = calculationParams.get_simulation_size()
-		start_time = maxi(self.start_time, calculationParams.begin)
+		start_time = tp["start_time"]
 		start_step = int(round((start_time - calculationParams.begin) / calculationParams.step_size))
+		end_step = int(round((tp["end_time"] - calculationParams.begin) / calculationParams.step_size))
+		power_curve = self._calcul_liste_puissance_charge(calculationParams)
 		xpar = xpars[0]
 		ypar = ypars[0]
 		for i in range(self._get_minimizing_variables_count(calculationParams)):
-			for j in range(len(self.profile)):
-				tofill[start_step + ypar + i + j, xpar + i] = -self.profile[j]
+			for j in range(tp["max_charge_time"] // calculationParams.step_size):
+				if (start_step + i + j < sim_size and start_step + i + j < end_step ):
+					tofill[start_step + ypar + i + j, xpar + i] = power_curve[j]
+					print(i + j + start_step,i,j, start_step, end_step, power_curve[j])
+				else:
+					print(i,j)
 
 	def _fill_functionnal_constraints(self, calculationParams: CalculationParams, tofill: np.ndarray, xpar: int, ypar: int):
 		self._make_machine_possible(calculationParams)
@@ -107,19 +134,24 @@ class VehicleConsumer(Consumer_interface):
 			tofill[ypar, xpar + x] = 1
 
 	def _get_consumption_curve(self, calculationParams : CalculationParams, variables : List[float]) -> np.ndarray:
+		tp = self._get_calculated_time_parameters(calculationParams)
 		sim_size = calculationParams.get_simulation_size()
 		toReturn = np.zeros((sim_size,), np.float64)
-		start_time = maxi(self.start_time, calculationParams.begin)
+		start_time = tp["start_time"]
 		start_step = int(round((start_time - calculationParams.begin) / calculationParams.step_size))
+		end_step = int(round((tp["end_time"] - calculationParams.begin) / calculationParams.step_size))
+		power_curve = self._calcul_liste_puissance_charge(calculationParams)
 		for i in range(len(variables)):
 			if (variables[i] != 0):
-				for j in range(len(self.profile)):
-					toReturn[start_step + i + j] = variables[i] * self.profile[j]
+				for j in range(len(power_curve)):
+					if (start_step + i + j < sim_size and start_step + i + j < end_step):
+						toReturn[start_step + i + j] = - variables[i] * power_curve[j]
 		return toReturn
 	def _get_decisions(self, calculationParams : CalculationParams, variables : List[float]) -> np.ndarray:
+		tp = self._get_calculated_time_parameters(calculationParams)
 		sim_size = calculationParams.get_simulation_size()
 		toReturn = np.zeros((sim_size,), np.int64)
-		start_time = maxi(self.start_time, calculationParams.begin)
+		start_time = tp["start_time"]
 		start_step = int(round((start_time - calculationParams.begin) / calculationParams.step_size))
 		for i in range(len(variables)):
 			if (variables[i] != 0):
