f8464be232b3c5f9f60090e5157fa44e99abab89
laennocode <laennocode@gmail.com>
2023-04-21 14:02:35 +0200

    modif of periods

--- elfe_interfaces/ELFE_data_gatherer.py
+++ elfe_interfaces/ELFE_data_gatherer.py
@@ -52,7 +52,7 @@ def get_merged_periods_and_has_changed(periods : List[Period]) -> Tuple[List[Per
 				continue
 			period_2 = periods[j]	
 			if period_2.start >= period_1.start and period_2.start < period_2.end:
-				print("merging", period_1, "and",period_2)
+				print("merging", period_1 - periods[0].start, "and",period_2 - periods[0].start)
 				start = period_1.start
 				end = max(period_1.end, period_2.end)
 				periods_to_return.append(Period(start, end))
