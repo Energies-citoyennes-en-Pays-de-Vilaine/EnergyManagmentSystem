a4d09604494dc86565ea382d3fc8de6928c17dca
laennocode <laennocode@gmail.com>
2023-04-26 13:28:34 +0200

    fixed problem in calculation of machine + add logging + reorganisation

--- solution/ConsumerTypes/MachineConsumer.py
+++ solution/ConsumerTypes/MachineConsumer.py
@@ -3,6 +3,13 @@ from typing import *
 from solution.Consumer_interface import Consumer_interface
 from solution.Calculation_Params import CalculationParams
 from solution.Utils.utils import maxi, mini
+
+from typing import TypedDict
+class _CalculatedTimeParametersMachine(TypedDict):
+	start_time          : int
+	end_time            : int
+	steps_count         : int
+
 class MachineConsumer(Consumer_interface):
     def __init__(self, id, profile, start_time, end_time, machine_count = 1):
         self.id = id
@@ -36,19 +43,52 @@ class MachineConsumer(Consumer_interface):
     def _get_functionnal_constraints_boundaries(self, calculationParams : CalculationParams) -> List[List[List[float]]]:
         self._make_machine_possible(calculationParams)
         return [[self.machine_count], [self.machine_count]]
-    def _make_machine_possible(self, calculationParams: CalculationParams):
+
+    def _get_constraints_repr(self, calculationParams : CalculationParams) -> str:
+        tp = self._get_calculated_time_parameters(calculationParams)
+        return f"Machine {self.id} (start={tp['start_time']}, end={tp['end_time']}, steps={tp['steps_count']})"
+    
+    def _get_calculated_time_parameters(self, calculationParams: CalculationParams) -> _CalculatedTimeParametersMachine:
         start_time = maxi(self.start_time, calculationParams.begin)
         end_time = mini(self.end_time, calculationParams.end + calculationParams.step_size)
+        step_count = len(self.profile)
+        return {
+            "start_time"  :start_time,
+            "end_time"    : end_time,
+            "steps_count" : step_count
+
+        }
+    
+    def _make_machine_possible(self, calculationParams: CalculationParams):
+        tp = self._get_calculated_time_parameters(calculationParams)
+        start_time = tp["start_time"]
+        end_time   = tp["end_time"]
+        
         if (start_time + len(self.profile) * calculationParams.time_delta  > end_time ):
-            print(f"warning, machine {self.id} impossible because user constraints doesn't allow it to fit, rescheduling end constraint")
+            print(f"warning, {self._get_constraints_repr(calculationParams)} impossible because user constraints doesn't allow it to fit, rescheduling end constraint")
             self.end_time = self.start_time + len(self.profile) * calculationParams.time_delta
+            tp = self._get_calculated_time_parameters(calculationParams)
+            start_time = tp["start_time"]
+            end_time   = tp["end_time"]
+            print(f"new {self._get_constraints_repr(calculationParams)}")
+
         if (start_time + len(self.profile) * calculationParams.time_delta > calculationParams.end):
-            print(f"warning, machine {self.id} impossible because it doesn't fit before the end of simulation. making it earlier so it fits, will be rescheduled later")
+            print(f"warning, {self._get_constraints_repr(calculationParams)} impossible because it doesn't fit before the end of simulation. making it earlier so it fits, will be rescheduled later")
             self.start_time = calculationParams.end - len(self.profile) * calculationParams.time_delta
+            tp = self._get_calculated_time_parameters(calculationParams)
+            start_time = tp["start_time"]
+            end_time   = tp["end_time"]
+            print(f"new {self._get_constraints_repr(calculationParams)}")
+
         if (self.end_time <= calculationParams.begin):
-            print(f"warning, machine {self.id} impossible because it's supposed to end before the start of simulation. scheduling it for next step")
+            print(f"warning, {self._get_constraints_repr(calculationParams)} impossible because it's supposed to end before the start of simulation. scheduling it for next step")
             self.start_time = calculationParams.begin
             self.end_time = self.start_time + len(self.profile) * calculationParams.time_delta
+            tp = self._get_calculated_time_parameters(calculationParams)
+            start_time = tp["start_time"]
+            end_time   = tp["end_time"]
+            print(f"new {self._get_constraints_repr(calculationParams)}")
+    
     def _get_minimizing_variables_count(self, calculationParams : CalculationParams) -> int:
         self._make_machine_possible(calculationParams)
         start_time = maxi(self.start_time, calculationParams.begin)
