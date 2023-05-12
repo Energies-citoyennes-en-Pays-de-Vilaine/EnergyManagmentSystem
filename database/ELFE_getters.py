4785e57a53bda4301cb3888fd14e827705b04e9c
laennocode <laennocode@gmail.com>
2023-05-12 18:32:45 +0200

    various query bugfix

--- database/ELFE_getters.py
+++ database/ELFE_getters.py
@@ -37,17 +37,17 @@ class ECSToScheduleType():
 	end            : int
 	equipment_type : int
 
-def get_ECS_to_schedule(credentials, ECS_not_to_schedule: Union[List[int], None] = None) -> List[ECSToScheduleType]:
+def get_ECS_to_schedule(credentials, timestamp, ECS_not_to_schedule: Union[List[int], None] = None) -> List[ECSToScheduleType]:
 	query = (sql.SQL(" SELECT epm.id, ecs.mesures_puissance_elec_id, machine.mesures_puissance_elec_id, machine.timestamp_de_fin_souhaite, machine.delai_attente_maximale_apres_fin, equipement.equipement_pilote_ou_mesure_type_id"
-			+ " FROM {0} AS machine" 
-	    	+ " INNER JOIN {1} AS cycle ON cycle.id = machine.cycle_equipement_pilote_machine_generique_id " 
-			+ " INNER JOIN {2} AS equipement ON machine.equipement_pilote_ou_mesure_id = equipement.id "
-			+ " WHERE equipement.equipement_pilote_ou_mesure_mode_id = %s;").format(
-				sql.Identifier(ELFE_database_names['ELFE_MachineGenerique']),
-				sql.Identifier(ELFE_database_names['ELFE_MachineGeneriqueCycle']),
-				sql.Identifier(ELFE_database_names['ELFE_EquipementPilote'])
+			+ " FROM {0} AS epm" 
+	    	+ " INNER JOIN {1} AS ecs ON epm.id=ecs.equipement_pilote_ou_mesure_id" 
+			+ " INNER JOIN {2} AS hc ON ecs.id = hc.equipement_pilote_ballon_ecs_id"
+			+ "WHERE hc.actif=true and epm.equipement_pilote_ou_mesure_mode_id=%s AND epm.timestamp_derniere_mise_en_marche + 12 * 3600 <= %s").format(
+				sql.Identifier(ELFE_database_names['ELFE_EquipementPilote']),
+				sql.Identifier(ELFE_database_names['ELFE_BallonECS']),
+				sql.Identifier(ELFE_database_names['ELFE_BallonECSHeuresCreuses'])
 			), 
-			[MODE_PILOTE])
+			[MODE_PILOTE, timestamp])
 	result = fetch(credentials, query)
 	result_typed : List[ECSToScheduleType] = [ECSToScheduleType(r[0], r[1], r[2], r[3], r[4], r[5], r[6]) for r in result]
 	
