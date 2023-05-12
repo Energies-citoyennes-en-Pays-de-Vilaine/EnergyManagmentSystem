854479d2f8bc795f89af65bba657dd8376249774
laennocode <laennocode@gmail.com>
2023-05-12 18:09:52 +0200

    remove of fstrings causing crashes

--- database/ELFE_getters.py
+++ database/ELFE_getters.py
@@ -38,11 +38,11 @@ class ECSToScheduleType():
 	equipment_type : int
 
 def get_ECS_to_schedule(credentials, ECS_not_to_schedule: Union[List[int], None] = None) -> List[ECSToScheduleType]:
-	query = (sql.SQL(f" SELECT epm.id, ecs.mesures_puissance_elec_id, machine.mesures_puissance_elec_id, machine.timestamp_de_fin_souhaite, machine.delai_attente_maximale_apres_fin, equipement.equipement_pilote_ou_mesure_type_id"
-			+ f" FROM {0} AS machine" 
-	    	+ f" INNER JOIN {1} AS cycle ON cycle.id = machine.cycle_equipement_pilote_machine_generique_id " 
-			+ f" INNER JOIN {2} AS equipement ON machine.equipement_pilote_ou_mesure_id = equipement.id "
-			+ f" WHERE equipement.equipement_pilote_ou_mesure_mode_id = %s;").format(
+	query = (sql.SQL(" SELECT epm.id, ecs.mesures_puissance_elec_id, machine.mesures_puissance_elec_id, machine.timestamp_de_fin_souhaite, machine.delai_attente_maximale_apres_fin, equipement.equipement_pilote_ou_mesure_type_id"
+			+ " FROM {0} AS machine" 
+	    	+ " INNER JOIN {1} AS cycle ON cycle.id = machine.cycle_equipement_pilote_machine_generique_id " 
+			+ " INNER JOIN {2} AS equipement ON machine.equipement_pilote_ou_mesure_id = equipement.id "
+			+ " WHERE equipement.equipement_pilote_ou_mesure_mode_id = %s;").format(
 				sql.Identifier(ELFE_database_names['ELFE_MachineGenerique']),
 				sql.Identifier(ELFE_database_names['ELFE_MachineGeneriqueCycle']),
 				sql.Identifier(ELFE_database_names['ELFE_EquipementPilote'])
