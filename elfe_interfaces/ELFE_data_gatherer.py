2330a4e388862c0ef91d703d85dc80ed86eb8eca
laennocode <laennocode@gmail.com>
2023-05-16 15:50:42 +0200

    tesst

--- elfe_interfaces/ELFE_data_gatherer.py
+++ elfe_interfaces/ELFE_data_gatherer.py
@@ -221,6 +221,7 @@ def get_heater_consumer(timestamp : int, calculationParams: CalculationParams) -
 				target_temperature_high[i] = t_high_comfort
 		t_init = 0.0
 		initial_state = False
+		print("get id ", heater.modele_thermique_id)
 		m_th_query = fetch(db_credentials["EMS"], ("SELECT * FROM ems_modele_thermique where id=%s", [heater.modele_thermique_id]))
 		print(m_th_query)
 		m_th : EMS_Modele_Thermique = EMS_Modele_Thermique.create_from_select_output(m_th_query[0]) 
